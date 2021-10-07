51d142cef210c3f2ccc87e23ee9f737d0c0e9cb9
Vlad <vberezhn@softserveinc.com>
2021-10-07 19:02:04 +0300

    added basic Category validation and tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :categories
+
+  test "category attributes must not be empty" do
+    category = Category.new
+    assert category.invalid?
+    assert category.errors[:name].any?
+  end
+
+  test "category is not valid without unique name" do
+    category = Category.new(name: categories(:one).name)
+    assert category.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], category.errors[:name]
+  end
 end
