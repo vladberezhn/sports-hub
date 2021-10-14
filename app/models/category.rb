64765da7978836f57b69643b2e5001ade5d2f300
Vlad <vberezhn@softserveinc.com>
2021-10-14 17:03:57 +0300

    fix: category foreign key

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 #
 class Category < ApplicationRecord
-  has_many :subcategories
+  has_many :subcategories, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
--- test/controllers/subcategories_controller_test.rb
+++ test/controllers/subcategories_controller_test.rb
@@ -17,7 +17,7 @@ class SubcategoriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create subcategory" do
     assert_difference('Subcategory.count') do
-      post subcategories_url, params: { subcategory: { category_id: @subcategory.category_id, name: "#{@subcategory.name}unique" } }
+      post subcategories_url, params: { subcategory: { category_id: @subcategory.category.id, name: "#{@subcategory.name}unique" } }
     end
 
     assert_redirected_to subcategory_url(Subcategory.last)
@@ -34,7 +34,7 @@ class SubcategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update subcategory" do
-    patch subcategory_url(@subcategory), params: { subcategory: { category_id: @subcategory.category_id, name: "#{@subcategory.name}uniquenew" } }
+    patch subcategory_url(@subcategory), params: { subcategory: { category_id: @subcategory.category.id, name: "#{@subcategory.name}uniquenew" } }
     assert_redirected_to subcategory_url(@subcategory)
   end
 
