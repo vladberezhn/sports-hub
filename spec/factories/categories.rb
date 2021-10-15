70eaf69f23945e27b9ef04b0988f9249a51ff3f3
Vlad <vberezhn@softserveinc.com>
2021-10-15 18:32:50 +0300

    added category creation with subcategories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -4,4 +4,16 @@ FactoryBot.define do
   factory :category do
     name { 'NHL' }
   end
+
+  factory :subcategory do
+    name { 'NHC South' }
+    category
+  end
+end
+
+def category_with_subcategories
+  FactoryBot.create(:category) do |category|
+    FactoryBot.create(:subcategory, category: category)
+    FactoryBot.create(:subcategory, name: 'NHC North', category: category)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,11 +4,16 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   before(:all) do
-    @category1 = create(:category)
-    # @category2 = create(:category, name: 'NBA')
+    # @category1 = create(:category)
+    @category1 = category_with_subcategories
+    @category2 = create(:category, name: 'NBA')
   end
 
   it 'should be valid' do
     expect(@category1).to be_valid
+  end
+
+  it 'should be valid' do
+    expect(@category2).to be_valid
   end
 end
