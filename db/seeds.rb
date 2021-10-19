7fac299f9ffbd7a0c6698b87818eff37bc52f530
Vlad <vberezhn@softserveinc.com>
2021-10-19 17:36:47 +0300

    added db seed for categories and subcategories; removed extra category specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@category1 = Category.create({ name: 'NFL' })
+subcategories1 = ['AFC East', 'AFC North', 'AFC South', 'AFC West', 'NFC East', 'NFC North', 'NFC South', 'NFC West']
+subcategories1.each { |subcategory| @category1.subcategories.create({ name: subcategory }) }
+
+Category.create({ name: 'NBA' })
+Category.create({ name: 'MLB' })
+Category.create({ name: 'NHL' })
+Category.create({ name: 'CBB' })
+Category.create({ name: 'CFB' })
+Category.create({ name: 'Nascar' })
+Category.create({ name: 'Golf' })
+Category.create({ name: 'Soccer' })
+Category.create({ name: 'Team Hub' })
+Category.create({ name: 'Lifestyle' })
+Category.create({ name: 'Dealbook' })
+Category.create({ name: 'Video' })
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -11,17 +11,6 @@ RSpec.describe Category, type: :model do
     # @category1 = create(:category)
     @category1 = category_with_subcategories
     @category2 = create(:category, name: 'NBA')
-    @category3 = create(:category, name: 'MLB')
-    @category4 = create(:category, name: 'NHL')
-    @category5 = create(:category, name: 'CBB')
-    @category6 = create(:category, name: 'CFB')
-    @category7 = create(:category, name: 'Nascar')
-    @category8 = create(:category, name: 'Golf')
-    @category9 = create(:category, name: 'Soccer')
-    @category10 = create(:category, name: 'Team Hub')
-    @category11 = create(:category, name: 'Lifestyle')
-    @category12 = create(:category, name: 'Dealbook')
-    @category13 = create(:category, name: 'Video')
   end
 
   it 'should be valid' do
@@ -37,49 +26,5 @@ RSpec.describe Category, type: :model do
     @category1.subcategories.each do |subcategory|
       expect(subcategory).to be_valid
     end
-  end
-
-  it 'should be valid' do
-    expect(@category3).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category4).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category5).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category6).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category7).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category8).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category9).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category10).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category11).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category12).to be_valid
-  end
-
-  it 'should be valid' do
-    expect(@category13).to be_valid
   end
 end
