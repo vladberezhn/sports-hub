6eb54bc81dd781e298c3d016fe4c09f45cb74f6a
Vlad <vberezhn@softserveinc.com>
2021-10-20 16:00:21 +0300

    refactored following review

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,5 @@
 subcategories1 = ['AFC East', 'AFC North', 'AFC South', 'AFC West', 'NFC East', 'NFC North', 'NFC South', 'NFC West']
 subcategories1.each { |subcategory| @category1.subcategories.create({ name: subcategory }) }
 
-Category.create({ name: 'NBA' })
-Category.create({ name: 'MLB' })
-Category.create({ name: 'NHL' })
-Category.create({ name: 'CBB' })
-Category.create({ name: 'CFB' })
-Category.create({ name: 'Nascar' })
-Category.create({ name: 'Golf' })
-Category.create({ name: 'Soccer' })
-Category.create({ name: 'Team Hub' })
-Category.create({ name: 'Lifestyle' })
-Category.create({ name: 'Dealbook' })
-Category.create({ name: 'Video' })
+categories = ['NBA', 'MLB', 'NHL', 'CBB', 'CFB', 'Nascar', 'Golf', 'Soccer', 'Team Hub', 'Lifestyle', 'Dealbook', 'Video']
+categories.each { |category| Category.create({ name: category }) }
--- spec/requests/page_spec.rb
+++ spec/requests/page_spec.rb
@@ -1,7 +1,1 @@
 require 'rails_helper'
-
-RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
