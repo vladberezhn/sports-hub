86b809ce59b56d60ca79350a3601c82c01ef3472
Vlad <vberezhn@softserveinc.com>
2021-10-25 16:04:42 +0300

    fix for factory category category_with_subcategories function

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -11,20 +11,3 @@ FactoryBot.define do
     category
   end
 end
-
-# require '../support/categories'
-def category_with_subcategories
-  FactoryBot.create(:category) do |category|
-    FactoryBot.create(:subcategory, category: category)
-    subcategories = ['AFC North',
-                     'AFC South',
-                     'AFC West',
-                     'NFC East',
-                     'NFC North',
-                     'NFC South',
-                     'NFC West']
-    subcategories.each do |subcategory|
-      FactoryBot.create(:subcategory, name: subcategory, category: category)
-    end
-  end
-end
