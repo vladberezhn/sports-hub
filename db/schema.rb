dd929f4d07ac994daef4fd3d7cb056f2430b7e27
Vlad <vberezhn@softserveinc.com>
2021-10-28 11:57:58 +0300

    removed admin_ tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_10_25_141813) do
 
-  create_table "admin_articles", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "admin_comments", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
