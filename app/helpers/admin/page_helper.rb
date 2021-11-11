64ba2ce424d9a5d4edbc83ee8f6632954b97b49e
Vlad <vberezhn@softserveinc.com>
2021-11-11 16:10:56 +0200

    fix helper method name

--- app/helpers/admin/page_helper.rb
+++ app/helpers/admin/page_helper.rb
@@ -1,5 +1,5 @@
 module Admin::PageHelper
-  def block_or_activate_user_href(user)
+  def block_or_activate_user_link(user)
     unless user.role.eql?('admin')
       if user.account_active?
         link_to('Block', admin_block_user_url(user.id))
