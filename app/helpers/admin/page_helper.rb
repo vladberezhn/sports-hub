b65da622b4a50eb11868791e9cddc924a7dbc575
Vlad <vberezhn@softserveinc.com>
2021-11-11 16:06:27 +0200

    fix. added to admin helper

--- app/helpers/admin/page_helper.rb
+++ app/helpers/admin/page_helper.rb
@@ -1,2 +1,11 @@
 module Admin::PageHelper
+  def block_or_activate_user_href(user)
+    unless user.role.eql?('admin')
+      if user.account_active?
+        link_to('Block', admin_block_user_url(user.id))
+      else
+        link_to('Activate', admin_activate_user_url(user.id))
+      end
+    end
+  end
 end
